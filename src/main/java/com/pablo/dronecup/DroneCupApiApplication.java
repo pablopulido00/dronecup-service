fcc5c9f45e05ec7c02a8f080d3a3aaf5a77e1350
pablopulido00 <pablopulidosolis@gmail.com>
2025-11-10 16:58:31 +0100

    Inizializa proyecto DroneCup con configuracion base

---
+++ src/main/java/com/pablo/dronecup/DroneCupApiApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.dronecup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneCupApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneCupApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablo/dronecup/DroneCupApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablo.dronecup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneCupApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
