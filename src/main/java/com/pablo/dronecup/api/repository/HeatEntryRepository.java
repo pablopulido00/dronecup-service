5fb0b36adf800f9e0e3f7cb5d2806b02251efd72
pablopulido00 <pablopulidosolis@gmail.com>
2026-01-13 11:46:59 +0100

    feat: añadir metodos existsByHeatIdAndPilotIs y existsByHeatIdAndStartPosition

--- src/main/java/com/pablo/dronecup/api/repository/HeatEntryRepository.java
+++ src/main/java/com/pablo/dronecup/api/repository/HeatEntryRepository.java
@@ -5,4 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HeatEntryRepository extends JpaRepository<HeatEntry, Long> {
 
+    boolean existsByHeatIdAndPilotId(Long heatId, Long pilotId);
+
+    boolean existsByHeatIdAndStartPosition (Long heatId, Integer StartPosition);
+
 }
